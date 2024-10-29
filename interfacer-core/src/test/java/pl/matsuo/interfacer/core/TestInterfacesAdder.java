a69d475c0f76a89098c04969f4690b45a2e6c2a0
Antony John <antonyjohn@4pm.ie>
2024-10-29 14:57:26 +0530

    Added Tests for multi interface inheritance for a class

--- interfacer-core/src/test/java/pl/matsuo/interfacer/core/TestInterfacesAdder.java
+++ interfacer-core/src/test/java/pl/matsuo/interfacer/core/TestInterfacesAdder.java
@@ -5,11 +5,11 @@ import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -18,35 +18,43 @@ import pl.matsuo.interfacer.model.ifc.IfcResolve;
 import pl.matsuo.interfacer.showcase.GenericInterface;
 import pl.matsuo.interfacer.showcase.HasName;
 import pl.matsuo.interfacer.showcase.MutableOwner;
+import pl.matsuo.interfacer.showcase.SampleInterface;
 
 @Slf4j
 public class TestInterfacesAdder {
 
   @Test
-  public void testHasName() {
-    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications =
-        doTestInterface("/classes/test/SampleClass.java", HasName.class);
+  public void testHasNameApplied() {
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = doTestInterface(
+        "/classes/test/SampleClass.java", HasName.class);
     assertEquals(1, modifications.size());
   }
 
   @Test
+  public void testAdditionalMatchedInterfaceApplied() {
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = doTestInterface(
+        "/classes/test/SampleClass.java", HasName.class, SampleInterface.class);
+    assertEquals(2, modifications.size());
+  }
+
+  @Test
   public void testGenericInterface() {
-    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications =
-        doTestInterface("/classes/test/SampleGenericClass.java", GenericInterface.class);
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = doTestInterface(
+        "/classes/test/SampleGenericClass.java", GenericInterface.class);
     assertEquals(1, modifications.size());
   }
 
   @Test
   public void testNotGenericInterface() {
-    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications =
-        doTestInterface("/classes/test/SampleNotGenericClass.java", GenericInterface.class);
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = doTestInterface(
+        "/classes/test/SampleNotGenericClass.java", GenericInterface.class);
     assertEquals(0, modifications.size());
   }
 
   @Test
   public void testMutableOwner() {
-    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications =
-        doTestInterface("/classes/test/SampleMutableClass.java", MutableOwner.class);
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = doTestInterface(
+        "/classes/test/SampleMutableClass.java", MutableOwner.class);
     assertEquals(1, modifications.size());
   }
 
@@ -64,23 +72,58 @@ public class TestInterfacesAdder {
     ParsingContext parsingContext = new ParsingContext(emptyList(), scanDir, scanDir);
 
     ClasspathInterfacesScanner interfacesScanner = new ClasspathInterfacesScanner();
-    IfcResolve genericInterface =
-        interfacesScanner.processClassFromClasspath(ifc, parsingContext.typeSolver);
+    IfcResolve genericInterface = interfacesScanner.processClassFromClasspath(ifc, parsingContext.typeSolver);
 
     assertNotNull(genericInterface);
 
-    ParseResult<CompilationUnit> compilationUnitParseResult =
-        interfacesAdder.parseFile(parsingContext.javaParser, sampleClassFile);
+    ParseResult<CompilationUnit> compilationUnitParseResult = interfacesAdder.parseFile(parsingContext.javaParser,
+        sampleClassFile);
 
     if (!compilationUnitParseResult.isSuccessful()) {
       log.info("" + compilationUnitParseResult.getProblems());
     }
 
-    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications =
-        interfacesAdder.processAllFiles(
-            asList(compilationUnitParseResult),
-            asList(genericInterface),
-            parsingContext.javaParser);
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = interfacesAdder.processAllFiles(
+        asList(compilationUnitParseResult),
+        asList(genericInterface),
+        parsingContext.javaParser);
+
+    modifications.forEach(mod -> log.info(mod.toString()));
+
+    return modifications;
+  }
+
+  public List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> doTestInterface(String className,
+      Class<?> ... ifcs) {
+    InterfacesAdder interfacesAdder = new InterfacesAdder();
+
+    File sampleClassFile = fileForResource(className);
+    File scanDir = sampleClassFile.getParentFile();
+
+    ParsingContext parsingContext = new ParsingContext(emptyList(), scanDir, scanDir);
+
+    ClasspathInterfacesScanner interfacesScanner = new ClasspathInterfacesScanner();
+    List<IfcResolve> ifcResolves = new ArrayList<>();
+    for (Class<?> ifc : ifcs) {
+      IfcResolve ifcResolve = interfacesScanner.processClassFromClasspath(ifc, parsingContext.typeSolver);
+      if (ifcResolve != null) {
+        ifcResolves.add(ifcResolve);
+      }
+    }
+    assertNotNull(ifcResolves);
+    assertFalse(ifcResolves.isEmpty());
+
+    ParseResult<CompilationUnit> compilationUnitParseResult = interfacesAdder.parseFile(parsingContext.javaParser,
+        sampleClassFile);
+
+    if (!compilationUnitParseResult.isSuccessful()) {
+      log.info("" + compilationUnitParseResult.getProblems());
+    }
+
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = interfacesAdder.processAllFiles(
+        asList(compilationUnitParseResult),
+        ifcResolves,
+        parsingContext.javaParser);
 
     modifications.forEach(mod -> log.info(mod.toString()));
 
@@ -101,16 +144,15 @@ public class TestInterfacesAdder {
 
     ParsingContext parsingContext = new ParsingContext(emptyList(), scanDir, interfacesDir);
 
-    ParseResult<CompilationUnit> compilationUnitParseResult =
-        interfacesAdder.parseFile(parsingContext.javaParser, sampleClassFile);
+    ParseResult<CompilationUnit> compilationUnitParseResult = interfacesAdder.parseFile(parsingContext.javaParser,
+        sampleClassFile);
 
     if (!compilationUnitParseResult.isSuccessful()) {
       log.info("" + compilationUnitParseResult.getProblems());
     }
 
-    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications =
-        interfacesAdder.processAllFiles(
-            asList(compilationUnitParseResult), ifcResolves, parsingContext.javaParser);
+    List<Pair<IfcResolve, ClassOrInterfaceDeclaration>> modifications = interfacesAdder.processAllFiles(
+        asList(compilationUnitParseResult), ifcResolves, parsingContext.javaParser);
 
     modifications.forEach(mod -> log.info(mod.toString()));
 
---
+++ sample-interfaces/src/main/java/pl/matsuo/interfacer/showcase/SampleInterface.java
@@ -0,0 +1,10 @@
+package pl.matsuo.interfacer.showcase;
+
+public interface SampleInterface {
+
+    Integer getValue();
+
+    String getDescription();
+
+    void update(String content);
+}
